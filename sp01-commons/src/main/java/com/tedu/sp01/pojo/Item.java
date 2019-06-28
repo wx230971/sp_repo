85a7da1375e55b4223e395960e3edf1bc3120e4c
Administrator <Administrator@USER-20190220CH>
2019-06-28 09:52:54 +0800

    22

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -8,11 +8,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Item {
-	//编号AAA
+	//缂栧彿AAA
 	private Integer id;
 	private String name;
 	private Integer number;
 	//大数据库的哈手机客
 	 //hjdkasjk
-	//8978912
 }
